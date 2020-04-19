fae5268a74c40dc43f3f762512974202d35c2cf0
Junaid Sikander <Junaid_sikander@live.com>
2020-04-19 18:56:52 +0500

    Going through our folder structure for the project, breaking out homepage component into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Diretory from '../../components/directory/directory.components'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Diretory/>
+     </div>
+
+
+);
+
+export default HomePage;
