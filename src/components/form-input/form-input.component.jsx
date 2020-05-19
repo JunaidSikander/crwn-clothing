e2e174ef27d8eee9dbb582fd121672a31816f682
Junaid Sikander <Junaid_sikander@live.com>
2020-05-19 17:28:26 +0500

    [FIX] styling issue

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import './form-input.styles.scss'
+import './form-input.styles.scss';
 
-const FormInput = ({label, onChange, ...otherProps}) => (
+const FormInput = ({ handleChange, label, ...otherProps }) => (
     <div className='group'>
-        <input className='form-input' onChange={onChange} {...otherProps}/>
-        {
-            label ?
-                (<label className={`${otherProps.value.length ? 'shrinks' : ''}  form-input-label`}>
-                    {label}
-                </label>)
-                : null
-        }
+        <input className='form-input' onChange={handleChange} {...otherProps} />
+        {label ? (
+            <label
+                className={`${
+                    otherProps.value.length ? 'shrink' : ''
+                } form-input-label`}
+            >
+                {label}
+            </label>
+        ) : null}
     </div>
 );
 
-export default FormInput
+export default FormInput;
