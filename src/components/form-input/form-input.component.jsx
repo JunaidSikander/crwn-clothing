1c4e77b80c165721b17a1b5031b4bd9f041cda83
Junaid Sikander <Junaid_sikander@live.com>
2020-05-19 17:36:53 +0500

    [FIX] onChange function issue

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './form-input.styles.scss';
 
-const FormInput = ({ handleChange, label, ...otherProps }) => (
+const FormInput = ({ onChange, label, ...otherProps }) => (
     <div className='group'>
-        <input className='form-input' onChange={handleChange} {...otherProps} />
+        <input className='form-input' onChange={onChange} {...otherProps} />
         {label ? (
             <label
                 className={`${
